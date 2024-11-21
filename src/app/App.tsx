1e0e09e948fdc8be04be91c8ab1652bd9e9b7b1b
Andrii Zhuhan <154527656+Anres47@users.noreply.github.com>
2024-11-21 20:37:12 +0100

    finished canvas

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,5 @@
-import { Canvas } from '../shared/ui/Canvas';
-
 function App() {
-	return (
-		<Canvas>
-			<h1 className="text-[65px] font-bold">New Task</h1>
-		</Canvas>
-	);
+	return <div></div>;
 }
 
 export default App;
--- src/shared/ui/Canvas.tsx
+++ src/shared/ui/Canvas.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
-export function Canvas({ children }: { children?: React.ReactNode }) {
+type CanvasProps = {
+	children: React.ReactNode;
+	width?: string;
+};
+
+export function Canvas({ children, width }: CanvasProps) {
 	return (
-		<div className="flex justify-center items-center w-[100vw] h-[100wh] bg-black/70">
-			<div className="w-[1032px] h-[977px] p-[53px] bg-[#fff] flex flex-col items-center ">{children}</div>
+		<div className="absolute bg-black/70">
+			<div className="flex justify-center items-center w-[100vw] h-[100vh]">
+				<div className="p-[53px] bg-[#fff] flex flex-col items-center " style={{ width: width }}>
+					{children}
+				</div>
+			</div>
 		</div>
 	);
 }
