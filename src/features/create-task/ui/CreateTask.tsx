3a357591e33617876ad0c9b6ddd14d92bfb0351e
Andrii Zhuhan <154527656+Anres47@users.noreply.github.com>
2024-11-23 20:06:32 +0100

    create-task - finished work

--- src/features/create-task/ui/CreateTask.tsx
+++ src/features/create-task/ui/CreateTask.tsx
@@ -1,15 +1,7 @@
 import { useState } from 'react';
 import { Canvas, MainButton, FormTitle, TextInput, TextArea, DateInput, TimeInput } from '../../../shared/ui';
-import { SelectColor } from '../../SelectColor';
+import { SelectColor, CloseButton } from '../../index';
 import { colors } from './model/data/colors';
-
-export function CloseButton({ onClick }: { onClick: () => void }) {
-	return (
-		<div className="absolute right-0 top-[50%] translate-y-[-50%] ">
-			<img onClick={onClick} className="transition-transform duration-200 ease-in-out hover:scale-105 active:scale-95 active:transition cursor-pointer" src="/icons/close-button.svg" alt="close-button" />
-		</div>
-	);
-}
 
 export function CreateTask() {
 	const [activeIndex, setActiveIndex] = useState<number | null>(null);
