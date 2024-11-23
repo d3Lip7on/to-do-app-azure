e7b33e18570b69674ad52c8b26d94eca894b59f8
Andrii Zhuhan <154527656+Anres47@users.noreply.github.com>
2024-11-23 20:06:32 +0100

    create-task - finished work

--- src/shared/ui/Canvas.tsx
+++ src/shared/ui/Canvas.tsx
@@ -9,7 +9,7 @@ export function Canvas({ children, width }: CanvasProps) {
 	return (
 		<div className="absolute bg-black/70">
 			<div className="flex justify-center w-[100vw] h-[100vh]">
-				<div className="p-[53px] bg-[#fff] flex flex-col " style={{ width: width }}>
+				<div className="p-[53px] bg-[#fff] flex flex-col h-min" style={{ width: width }}>
 					{children}
 				</div>
 			</div>
