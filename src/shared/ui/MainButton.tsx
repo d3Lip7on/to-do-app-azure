4c4b74e7b33c6ad7f7847e03e8faa1c87c4062ba
Andrii Zhuhan <154527656+Anres47@users.noreply.github.com>
2024-11-23 17:51:04 +0100

    features/select-color - reset

--- src/shared/ui/MainButton.tsx
+++ src/shared/ui/MainButton.tsx
@@ -1,37 +1,13 @@
-// import React from 'react';
-
-// type MainButtonProps = {
-// 	width?: string;
-// 	children: React.ReactNode;
-// };
-
-// export function MainButton({ width, children }: MainButtonProps) {
-// 	return (
-// 		<button
-// 			style={{ width: width }}
-// 			className="flex justify-center items-center transition-transform duration-200 ease-in-out hover:scale-105 hover:bg-[#f0ed20] active:scale-95 active:transition w-[100%] p-[20px] h-[86px] bg-[#FBF868] text-[40px] font-bold"
-// 		>
-// 			{children}
-// 		</button>
-// 	);
-// }
 import React from 'react';
 
 type MainButtonProps = {
 	width?: string;
 	children: React.ReactNode;
-	onClick: () => void;
 };
 
-export function MainButton({ width, children, onClick }: MainButtonProps) {
+export function MainButton({ width, children }: MainButtonProps) {
 	return (
-		<button
-			onClick={() => {
-				onClick();
-			}}
-			style={{ width: width }}
-			className="flex justify-center items-center transition-transform duration-200 ease-in-out hover:scale-105 hover:bg-[#f0ed20] active:scale-95 active:transition w-[100%] p-[20px] h-[86px] bg-[#FBF868] text-[40px] font-bold"
-		>
+		<button style={{ width: width }} className="flex justify-center items-center transition-transform duration-200 ease-in-out hover:scale-105 hover:bg-[#f0ed20] active:scale-95 active:transition w-[100%] p-[20px] h-[86px] bg-[#FBF868] text-[40px] font-bold">
 			{children}
 		</button>
 	);
