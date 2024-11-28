9f8b1df93f7bfac800220ce685f94ac2c590bede
Andrii Zhuhan <154527656+Anres47@users.noreply.github.com>
2024-11-28 17:53:15 +0100

    edit-task - some changes

--- src/features/edit-task/ui/EditTask.tsx
+++ src/features/edit-task/ui/EditTask.tsx
@@ -1,10 +1,13 @@
-import { useState, useContext } from 'react';
+import { useContext } from 'react';
 import { Canvas, MainButton } from '../../../shared/ui';
+import { FormContext } from '../../../shared/ui/FormContext';
+import { TaskWindow } from '../../../shared/ui/TaskWindow';
 
 export function EditTask() {
-	const { textInputState, textAreaState, textDateState, textTimeState, activeColor } = useContext(For);
+	const { textInputState, textAreaState, textDateState, textTimeState, activeColor } = useContext(FormContext);
 	return (
 		<Canvas width="774px">
+			<TaskWindow />
 			<div className="flex gap-[7px]">
 				<MainButton
 					onClick={() => {
